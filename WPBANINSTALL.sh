c9ac8dd0cd8478d0fc2d953a07ec90ca9d6d646a
W8SOJIB <92366482+W8SOJIB@users.noreply.github.com>
2024-09-14 12:06:42 +0600

    Add files via upload

---
+++ WPBANINSTALL.sh
@@ -0,0 +1,26 @@
+apt update -y
+
+apt upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install wget -y
+
+pkg install git -y
+
+git clone https://github.com/W8SOJIB/W8WhatsAppBan
+
+cd W8WhatsAppBan
+
+unzip VenomBotTermux.zip
+
+cd VenomBotTermux
+
+wget https://github.com/W8SOJIB/W8WhatsAppBan/raw/main/WPBAN.sh 
+
+mv WPBAN.sh $HOME
+
+bash instalar.sh
+
